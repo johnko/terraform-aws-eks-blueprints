8a985d963f24df78359547410e66ee4c09d0f0ce
johnko <279736+johnko@users.noreply.github.com>
2025-11-16 21:36:58 -0500

    update for 21

--- patterns/multi-tenancy-with-teams/main.tf
+++ patterns/multi-tenancy-with-teams/main.tf
@@ -43,9 +43,9 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name                   = local.name
-  cluster_version                = "1.33"
-  cluster_endpoint_public_access = true
+  name                   = local.name
+  kubernetes_version     = "1.33"
+  endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
