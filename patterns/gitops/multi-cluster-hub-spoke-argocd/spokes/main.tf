e39b3c3abd79d35e7dab9f4a89ffb6198d2fc044
johnko <279736+johnko@users.noreply.github.com>
2025-11-16 22:30:37 -0500

    update for 21

--- patterns/gitops/multi-cluster-hub-spoke-argocd/spokes/main.tf
+++ patterns/gitops/multi-cluster-hub-spoke-argocd/spokes/main.tf
@@ -254,9 +254,9 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.9.0"
 
-  cluster_name                   = local.name
-  cluster_version                = local.cluster_version
-  cluster_endpoint_public_access = true
+  name                   = local.name
+  kubernetes_version     = local.cluster_version
+  endpoint_public_access = true
 
 
   vpc_id     = module.vpc.vpc_id
@@ -294,7 +294,7 @@ module "eks" {
     }
   }
   # EKS Addons
-  cluster_addons = {
+  addons = {
     eks-pod-identity-agent = {
       most_recent = true
     }
